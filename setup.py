6b772398235d6bc18061ab43b32f5acc20c94731
AniketDogra <30937571+AniketDogra@users.noreply.github.com>
2018-10-01 21:02:57 +0530

    Update setup.py

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 import cx_Freeze
 
 executables = [cx_Freeze.Executable("letsrace.py")]
-
+#Code for setup 
 cx_Freeze.setup(
 	name="Let's Race",
 	options={"build_exe":{"packages":["pygame"],"include_files":["bgimg.png","img.png","img1.png","jazz.wav","crash.wav"]}},
